d44e60504e413bb8439ba54bb15255624bca1211
Deivid Vale <deividrodriguesvale@gmail.com>
2025-07-11 18:18:13 -0400

    Initial opam project.

---
+++ test/test_expressions.ml
@@ -0,0 +1,39 @@
+(*
+
+Language Semantics and Implementation
+
+- syntax: (the way we write stuff)
+- semantics: (the meaning of programs)
+  1. small step semantics
+      => GIVE MEANING TO VERY SMALL ATOMIC CONSTRUCTS FOR THE LANGUAGE'S
+      SYNTAX AND LET THE PROGRAMMER DEFINE THE WHOLE MEANING OF A PROGRAM
+  2. PROCESS MEANING
+      a). interpreter (Python, Java and so on )
+            Reads the sentences and itself will "run" the semantics
+      b). compiler (OCaml, C/C++ and so on)
+            Reads the sentences and transform it into binary code (machine code).
+
+Program P =
+  for(int i ; i < 10 ; i++) {
+    print "I am at : " + i
+  }
+MEANS..=> "in a for loop we count i from 0 to 9, and print the number"
+
+- values in a language are the the very basic building blocks of data
+      e.g., int, booleans, lists, user defined datatypes... are values
+
+I : Expression -> Value -> Value
+
+*)
+
+type expr = Error | Num of int | Add of expr * expr
+
+(* 0 + 0 *)
+
+let example = Add (Num 0, Num 0)
+
+(* parser : string -> expr *)
+
+let interpreter (e : expr) (v : expr) : expr =
+  match (e, v) with
+  | (Num _, _) -> Error
