48f25a4e047a208de4b61dbc82871a4f323bb15f
Sid <siddharthsrig@gmail.com>
2025-08-08 17:03:46 -0400

    Test

--- lib/syntax/untyped.ml
+++ lib/syntax/untyped.ml
@@ -23,9 +23,47 @@ type expr =
 
 (* Exercise 0. Implement syntactical identity is a predicate. *)
 (* Notation 1.3.4 *)
+let rec equal_expr e1 e2: bool = 
+  match e1, e2 with 
+  | Var x, Var y -> x = y
+  | App(e1, r1), App(e2, r2) -> equal_expr e1 e2 && equal_expr r1 r2
+  | Lam(x1, body1), Lam(x2, body2) -> x1 = x2 && equal_expr body1 body2 
+  | _, _ -> false 
+
+let () =
+  let e1 = Lam ("x", App (Var "x", Var "x")) in
+  let e2 = Lam ("x", App (Var "x", Var "x")) in
+  let e3 = Lam ("y", App (Var "y", Var "u")) in
+  let e4 = Lam ("x", App (Var "x", Var "y")) in   
+
+  let result1 = equal_expr e1 e2 in
+  print_endline (string_of_bool result1);   
+
+  let result2 = equal_expr e3 e2 in
+  print_endline (string_of_bool result2);    
+
+  let result3 = equal_expr e4 e2 in
+  print_endline (string_of_bool result3)     
+
 
 (* Exercise 1. Implement subterms. *)
 (* Definition 1.3.5 *)
+let rec sub_term e : expr list  = 
+  match e with 
+  | Var _  ->  [e]
+  | App(e1, e2) -> e :: sub_term e1 @ sub_term e2 
+  | Lam(_, body) -> e :: sub_term body 
+ Create string of expr !!!!!!!!!!!!!!!! 
+let rec print_list lst = 
+  match lst with 
+  | [] -> () 
+  | h :: t -> print_endline( string_of_expr h); print_list t 
+let () =
+  let e1 = Lam ("x", App (Var "x", Var "x")) in
+  let result1 = sub_term e1 in 
+  print_list result1
+
+
 
 (* Ex. 2. Implement a predicate (function that the output type is boolean) for proper subterms.*)
 (* Definition 1.3.8 *)
@@ -37,33 +75,3 @@ type expr =
 (* Ex. 4. Implement a predicate for closed terms. *)
 (* Definition 1.4.3 *)
 
-
-
-
-
-
-
-(* Example, the identity on lambda calculus:
-  -
-  M  = λ x. (x (λ y . y)) => "the semantics": take the first argument 'x' and apply the identity to x.
-  M' = λ z. (z (λ y. y))  => "the semantics" : the same as M.
-
-   "The name of bound variables should not affect its semantics."
-   Intuition:
-   f(x) = x + 2 => "the semantics": "take an input, add 2 to this input"
-   f(z) = z + 2 => the same...
-   f(w) = ... and so on...
-
-   f(x) = x + 2 + y => "the semantics" :
-    "take the input and add it to 2 and an unknown constant y"
-   f'(x) = x + 2 + z => "take the input add it to 2 and z"
-
-*)
-
-(* Exercises for this type: *)
-(*
-1. Pretty printing.
-2. Notions of equality.
-    - Syntactic equality ≡
-    - Alpha equality =α
-*)
