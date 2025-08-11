50c03d1e742cdb41c4cb4ce51b9d4a1439ad94b3
Deivid Vale <deividrodriguesvale@gmail.com>
2025-08-11 17:42:44 -0400

    Small changes during meeting.

--- test/test_lambda.ml
+++ test/test_lambda.ml
@@ -1,12 +1,36 @@
-(*Create lambda calculas function/implement it here
-  What it should do: 
-   - Have Boolean logic, true or false
-   - Have if statements 
-*)
+open Syntax.Untyped
 
-let ltrue x y = x 
-let lfalse x y = y 
-let lif b z w = b z w 
+let e1 = Lam ("x", App (Var "x", Var "x"))
+let e2 = Lam ("x", App (Var "x", Var "x"))
+let e3 = Lam ("y", App (Var "y", Var "u"))
+let e4 = Lam ("x", App (Var "x", Var "y"))
+let e5 = App (Var "x", Var "x")
+let e6 = App (Var "x", Var "y")
 
-let () = print_endline (lif ltrue "Yes" "No")
-let () = print_endline (lif lfalse "Yes" "No")
+let id1 = Lam ("x", (Var "x"))
+let id2 = Lam ("y", (Var "y"))
+
+
+let () =
+  string_of_expr e1  |> print_endline ;
+  string_of_expr id2 |> print_endline
+  (* string_of_bool (equal_expr id1 id2) |> print_endline *)
+
+
+
+(* let () =
+  print_endline (string_of_bool (equal_expr e3 e2));
+  print_endline (string_of_bool (equal_expr e2 e1));
+  print_endline (string_of_bool (equal_expr e4 e1));;
+
+let () =
+  print_list (sub_term e1);
+  print_list (sub_term e2);
+  print_list (sub_term e3)
+
+
+let () =
+  print_endline (string_of_bool (is_subterm e5 e1));
+  print_endline (string_of_bool (is_subterm e6 e1));
+  print_endline (string_of_bool (is_proper_subterm e1 e2));
+  print_endline (string_of_bool (is_proper_subterm e1 e5)) *)
