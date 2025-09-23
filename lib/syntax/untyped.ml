542d894bfd00a70d3bb2ff0dfb0422e5fd6415fa
Sid <siddharthsrig@gmail.com>
2025-09-23 13:19:53 -0400

    Test

--- lib/syntax/untyped.ml
+++ lib/syntax/untyped.ml
@@ -112,7 +112,7 @@ let rec bound_vars exp =
   | Lam (x, body) -> x :: bound_vars body
 
 (* Ex. 4. Implement a predicate for closed terms. *)
-(* Definition 1.4.3 *)
+(** Definition 1.4.3 *)
 let combinator exp =
   match free_vars exp with
   | [] -> true
@@ -129,94 +129,27 @@ Hint: you need to also implement renaming (Def. 1.5.1)
 
  *)
 
-(* This function checks whether the new name nwn is valid
-(fresh = doesn't occur anywhere in in the expression).
-*)
-let rec rename_check (e:expr) (oldn: string) (nwn:string) : bool =
-  match e with
+
+let rec rename_check (e:expr) (oldn: string) (nwn:string) : bool = 
+  match e with 
   |Var x -> if x = oldn then x = nwn else false
   |App (x1, x2) -> rename_check x1 oldn nwn || rename_check x2 oldn nwn
   |Lam (x1, body) -> if x1 = oldn then x1 = nwn else false || rename_check body oldn nwn
 
-(* This function satisfy the following:
-    - it only returns true if the "name" argument is completely new.
-*)
-let rec is_fresh (e : expr) (name : string) : bool =
-  match e with
-  | Var x ->
-    if x = name then false else true
-  | App(l, r) ->
-    is_fresh l name && is_fresh r name
-  | Lam (x, e') ->
-    if x = name then false else is_fresh e' name
-
-(**
-[rename e oldn nwn] is the expression [e'] resulting from replacing {b}every{b}
-free occurrence of the name [oldn] with the argument name [nwm].
-
-@Error the function exists with error if the new name is not fresh.
-*)
-let rec rename (e: expr) (oldn: string) (nwn : string) : expr =
-  if is_fresh e nwn then
-    match e with
-    | (Var x) as v ->
-      if x = oldn then Var nwn else v
-    | App (l, r) -> App (rename l oldn nwn, rename r oldn nwn)
-    | Lam (x, body) ->
-      Lam (x, rename body oldn nwn)
-  else
-  failwith (
-    "Renaming impossible as the new name " ^
-    nwn ^
-    " is not fresh in " ^ (string_of_expr e)
-  )
+let rec rename (e: expr) (oldn: string) (nwn : string) : expr = 
+  if not (rename_check e oldn nwn) then e else 
+    match e with 
+    | Var x -> if x = oldn then Var nwn else Var x 
+    | App (x1, x2) -> App (rename x1 oldn nwn, rename x2 oldn nwn)
+    | Lam (x1, body) -> if x1 = oldn then Lam (nwn, rename body oldn nwn) else Lam (x1, rename body oldn nwn)  
 (* Has some wring thigns int eh function, the newname shoudl be fresh and should not appear in teh aplhaequivalent functions*)
 
-(* A function [gen_new_name e] of an expression e, should return a string such that it is a fresh name for e. *)
-(* Hint:
-  1. Compute the list of all names that appear in e, let's call it l.
-    This will give us a list of names that for sure are not fresh for e.
-  2. Compute the length of l, let's call it n.
-    Why? Because this will give us the number of names that exists.
-  3. Let the CANDIDATE for new name be : "x" ^ (string_of_int n) (which is exactly xn), let's call this x_n.
-  4. Test if x_n is in l.
-      - if x_n is in l then
-          let's try a new name with now, "x" ^ (string_of_int (n + 1))
-          and go back to 4 to test the new name
-      - else
-          return x_(n + ...)
-Main idea:
-  - since we increase the index by one each time we fail and there are finitely many variables in l,
-    what will happen is that at some point we will create a name, let's say, x_m for some m
-    that doesn't occur in l.
-
-You can test that for any expression e, is_fresh e (gen_new_name e) will be true.
-*)
-let gen_new_name (e : expr) : string =
-  (* let l = <compute the list of all names that appear in e> in *)
-  failwith "not implemented"
-
-
-
-
-let rec is_alpha (e1: expr) (e2: expr) : bool =
-  match (e1,e2) with
+let rec is_alpha (e1: expr) (e2: expr) : bool = 
+  match (e1,e2) with 
   | Var x, Var y -> x = y
   | App (x1, x2), App (y1,y2) -> is_alpha x1 y1 && is_alpha x2 y2
-  | Lam (x, e1), Lam (y, e2) ->
-    if x = y then
-      is_alpha e1 e2
-    else
-      (* Challange: how to generate a new name such that is_fresh of the new name that we generate will give true. *)
-      is_alpha e1 (rename e2 y x)
+  | Lam (x1, x2), Lam (y1, y2) -> if x1 = y1 then is_alpha x2 y2 else is_alpha x2 (rename y2 y1 x1)
   | _ -> false
-
-(*  *)
-
-(*  *)
-
-
-
 
 (*
 Ex. 6 - Implement Substitution: Def. 1.6.1.
