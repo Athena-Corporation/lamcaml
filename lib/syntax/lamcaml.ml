7ce96232ead4aece1e51c5adfc43f0eb3645e13a
Sid <siddharthsrig@gmail.com>
2026-01-26 17:33:11 -0500

    lamcaml additions

--- lib/syntax/lamcaml.ml
+++ lib/syntax/lamcaml.ml
@@ -1,3 +1,33 @@
+type expr  =
+  | Var of string
+  | App of expr * expr
+  | Lam of string * expr
+  | If of expr * expr * expr  
+  | Op of native_op
+  | Num of int
+  | Boul of bool
+
+and native_op =
+  | Add
+  | Sub
+
+let eval_Op op v1 v2 = 
+  match op, v1, v2 with 
+  | Add, Num x, Num y -> Num (x + y) 
+  | Sub, Num x, Num y -> Num (x - y)
+  | _ -> failwith "error"
+
+let rec string_of_expr e =
+  match e with
+  | Var x -> x
+  | App (e1, e2) -> "(" ^ string_of_expr e1 ^ "•" ^ string_of_expr e2 ^ ")"
+  | Lam (x, body) -> "λ" ^ x ^ "." ^ "(" ^ string_of_expr body ^ ")"
+  | Num x -> string_of_int x
+  | Boul x -> if x = true then "True" else "False"
+  | If (x, y, z) -> "if" ^ string_of_expr x ^ "then" ^ string_of_expr y ^ "else" ^ string_of_expr z 
+  | _ -> failwith "error"
+
+
 (*
 Primitive types:
 
