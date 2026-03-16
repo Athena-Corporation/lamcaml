29d00c95cd920569b1abe35e7d55404716221dfa
Deivid Vale <deividrodriguesvale@gmail.com>
2026-03-16 19:02:13 -0400

    Edits on meeting on March 16th.

--- lib/syntax/lamcaml.ml
+++ lib/syntax/lamcaml.ml
@@ -46,9 +46,18 @@ let rec string_of_expr e =
   | If(f, s, t) -> "If " ^ string_of_expr f ^ " then " ^ string_of_expr s ^ " else " ^ string_of_expr t
   | Bool(true) -> "True"
   | Bool(false) ->"False"
-  | Num x -> string_of_int x 
-  | _ -> failwith "Error"
-
+  | Num x -> string_of_int x
+  | UnaOp(o, e) -> string_of_unop o ^ "(" ^ string_of_expr e ^ ")"
+  | BinOp(o, e1, e2) -> "(" ^ string_of_expr e1 ^ string_of_binop o ^ string_of_expr e2 ^ ")"
+  and string_of_unop (o : una_ops) =
+    match o with
+    | Inv -> "-"
+    | Not -> "¬"
+  and string_of_binop (o : bin_ops) =
+    match o with
+    | Add -> "+"
+    | Geq -> "≥"
+    | Leq -> "≤"
 
 let rec gen_list (e:expr) : string list =
   match e with
@@ -69,81 +78,134 @@ let rec is_fresh (e : expr) (name : string) : bool =
   | If (f ,s, t) -> is_fresh f name && is_fresh s name && is_fresh t name
   | BinOp(_, f, s) -> is_fresh f name && is_fresh s name
   | UnaOp(_, f) -> is_fresh f name
-  | _ -> false      
-
+  | _ -> true
 
 let rename (e: expr) (oldn: string) (nwn: string) : expr =
   let rec aux_rename (expr: expr) (newn: string) : expr =
     match expr with
       | Var x as v -> (if x = oldn then Var newn else v)
-      | App (l, r) -> App (aux_rename l newn, aux_rename r newn)    
+      | App (l, r) -> App (aux_rename l newn, aux_rename r newn)
       | If (l, m , r) -> If( aux_rename l newn, aux_rename m newn, aux_rename r newn)
       | Lam (x, body) -> (if x = oldn then expr else Lam (x, aux_rename body newn))
-      | _ -> failwith "Type cannot be renamed"
+      | BinOp (o, arg1, arg2) -> BinOp (o, aux_rename arg1 newn, aux_rename arg2 newn)
+      | UnaOp(o, expr) -> UnaOp(o, aux_rename expr newn)
+      | Bool b -> Bool b
+      | Num n -> Num n
   in
-    if is_fresh  e nwn  then
+    if is_fresh e nwn then
       aux_rename e nwn
   else
     let u' = gen_new_name nwn (gen_list e) in
     if is_fresh e u' then aux_rename e u'
-    else 
+    else
       failwith "Fatal Error: Implementation of the function gen_new_name in module untyped.ml is not correct."
 
 (*               Substituion         Do Some Tests to See if it works *)
 
-let rec app_sub (s : subst) (t: expr) : expr = 
+let rec app_sub (s : subst) (t: expr) : expr =
   let lst_sub = List.rev_append (gen_list t) (gen_list (snd s)) in
-  match t with 
-  | Var x as v -> if x = (fst s) then (snd s) else v
-  | App(l, r) -> App(app_sub s l, app_sub s r) 
+  match t with
+  | Var x -> if x = (fst s) then (snd s) else t
+  | App(l, r) -> App(app_sub s l, app_sub s r)
   | If(f, m, t) -> If(app_sub s f, app_sub s m, app_sub s t)
-  | Lam(x, body) -> 
-      if x = (fst s) then t 
-      else 
+  | Lam(x, body) ->
+      if x = (fst s) then t
+      else
         let u = gen_new_name x ([fst s] @ lst_sub) in
-        let new_body = rename body x u in 
+        let new_body = rename body x u in
         assert (is_fresh body u);
         Lam(u, app_sub s new_body)
-  | BinOp(x, f, e) -> BinOp(x, app_sub s f, app_sub s e)
-  | UnaOp(x, f) -> UnaOp(x, app_sub s f)
-  | Bool _ as b -> b
-  | Num _ as n -> n                
+  | BinOp(o, f, e) -> BinOp(o, app_sub s f, app_sub s e)
+  | UnaOp(o, f) -> UnaOp(o, app_sub s f)
+  | Bool _ -> t
+  | Num _  -> t
 
 
-let rec beta (e : expr) : expr = 
+let rec beta (e : expr) : expr =
   match e with
   | Var _ as v -> v
   | Num _ as n -> n
   | Bool _ as b -> b
   | Lam (x, body) -> Lam (x, beta body)
 
-  | App (Lam (x, body), arg) -> 
-      let subst = (x, arg) in 
+  | App (Lam (x, body), arg) ->
+      let subst = (x, arg) in
       app_sub subst (beta body)
 
-  | BinOp (typ, l, r) -> 
-      (match (typ, beta l, beta r) with 
+  | BinOp (typ, l, r) ->
+      (match (typ, beta l, beta r) with
       | (Add, Num x, Num y) -> Num (x + y)
       | (Leq, Num x, Num y) -> Bool (x <= y)
       | (Geq, Num x, Num y) -> Bool (x >= y)
       | _ -> failwith "Type error")
-  
-  | UnaOp (typ, e) -> 
+
+  | UnaOp (typ, e) ->
     (match (typ, beta e) with
     | (Inv, Num x) -> Num (-x)
     | (Not, Bool x) -> if x = true then (Bool false) else (Bool true)
-    | _ -> failwith "Type error"  
+    | _ -> failwith "Type error"
     )
 
-  | If(f, s, t) -> 
+  | If(f, s, t) ->
     (match (beta f) with
     | Bool true -> beta s
-    | Bool false -> beta t 
+    | Bool false -> beta t
     | _ -> failwith "Type error")
 
   | App (l, r) -> App (beta l, beta r)
 
 
+
+
+(*
+Call-by-value (eager evaluation) semantics :
+  in every functional application f ∙ arg1 ∙ ... ∙ argn,you first reduce the arguments of the functions to values, then pass the arguments to the function... (intuition...)
+
+
+(β-axiom) for call-by-value (complete the rules...)
+small-step semantics for call-by-value
+
+        s v : expr    is_value(v) = true
+    ---------------------------------------------- (ax)
+              (λ x. s) ∙ v ->ᵥ s[x := v]
+
+          s t u : expr        is_value(s) = true
+    -------------------------------------------------- (If-True)
+          if s then t else u ->ᵥ t
+
+
+          s t u : expr        is_value(s) = false
+    -------------------------------------------------- (If-False)
+          if s then t else u ->ᵥ u
+
+          complete the rules...
+
+
+
+f(x) = x + 1
+f(2) = 2 + 1
+
+Computation example:
+1. Not-call-by-value
+  (λ x y. x + y + 1) ∙ (Num 1 + Num 2) (Num (1 + 0))
+  ->ᵦ (λ y. (Num 1 + Num 2) + y + 1) ∙ (Num (1 + 0))
+  ->ᵦ (λ x y. x + y + 1)
+
+2. Being call-by-value
+  (λ x y. x + y + 1) ∙ (Num 1 + Num 2) (Num (1 + 0))
+
+
+With If statements
+
+if x > 0 then ((λ x . 2^8) 3)/x else (λ x. 3^10000) 38338
+
+
+
+if ((λ x y. x + y + 1) 0 0 > 0) then
+  false
+else true
+
+*)
 
 (*
 Primitive types:
--- test/test_lamcaml.ml
+++ test/test_lamcaml.ml
@@ -7,16 +7,18 @@ let f2 = App(Var"u", Var "y")
 
 let f4 = App(Lam("x", Var "x"), Var "y")
 let e = If(BinOp(Geq, Num 2, Num 1), Var "x", Var "y")
-let u = If(UnaOp(Not,(BinOp(Geq, Num 2, Num 1))), Var "x", Var "y")
+let u = If(UnaOp(Not,(BinOp(Geq, Var "x", Num 1))), App(Var "x", Lam("x", Var "x")), Var "y")
 let f = BinOp(Add, Num 6, Num 7)
 let y = BinOp(Add, UnaOp(Inv,Num 6), Num 7)
 
+(* True [x := y] = True *)
 
-let()  = 
-  print_endline(string_of_expr(beta f4));
-  print_endline(string_of_expr(beta e)); 
+let()  =
+  print_endline ((rename u "x" "z") |> string_of_expr)
+  (* print_endline(is_fresh f1 "x" |> string_of_bool) *)
+  (* print_endline(string_of_expr(beta e));
   print_endline(string_of_expr(beta u));
   print_endline(string_of_expr(beta f));
-  print_endline(string_of_expr(beta y))
-   
-  
+  print_endline(string_of_expr(beta y)) *)
+
+
